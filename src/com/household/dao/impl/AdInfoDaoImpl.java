1587d3fd9b3bd9205f99a659ede4757cba427886
pengjie1998 <1135584436@qq.com>
2020-12-13 14:31:46 +0800

    彭杰  测试

--- src/com/household/dao/impl/AdInfoDaoImpl.java
+++ src/com/household/dao/impl/AdInfoDaoImpl.java
@@ -17,6 +17,7 @@ public class AdInfoDaoImpl implements AdInfoDao {
 	
 	//123456789
 	//987654321
+	//111111111
 	//创建  DBUtils 操作数据库的对象  (需要给定 数据源)
     QueryRunner qr = new QueryRunner(C3P0Utils.getDataSource());
 
